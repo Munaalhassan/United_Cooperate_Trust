6ecd886f5582447297a02d5a908803392180db2d
Munaalhassan <munaalhassan160@gmail.com>
2026-04-22 19:34:29 -0400

    feat: add tailwind utility helper and install date-fns dependency

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -1,3 +1,10 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+}
+
 export function timeAgo(date: string | Date): string {
     const now = new Date();
     const then = new Date(date);
